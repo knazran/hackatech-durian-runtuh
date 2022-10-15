d451439f8b89db052c4e757279984c63fa8e0228
Knazran <khairulnazran94@gmail.com>
2022-10-15 16:45:26 +0800

    Setup ionic-vue with tailwind

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,24 @@ import { IonicVue } from '@ionic/vue';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
+import 'tailwindcss/base.css'
+
 /* Basic CSS for apps built with Ionic */
 import '@ionic/vue/css/normalize.css';
 import '@ionic/vue/css/structure.css';
 import '@ionic/vue/css/typography.css';
 
+import 'tailwindcss/components.css'
+
 /* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
+// import '@ionic/vue/css/padding.css';
+// import '@ionic/vue/css/float-elements.css';
+// import '@ionic/vue/css/text-alignment.css';
+// import '@ionic/vue/css/text-transformation.css';
+// import '@ionic/vue/css/flex-utils.css';
+// import '@ionic/vue/css/display.css';
+
+import 'tailwindcss/utilities.css'
 
 /* Theme variables */
 import './theme/variables.css';
